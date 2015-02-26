54e63bf3d5ee86f45303f4843742738c923ec0b9
Kate <katebeavis84@gmail.com>
2015-02-26 09:05:34 +0000

    Tidied up, removed comments

--- lib/server.rb
+++ lib/server.rb
@@ -57,11 +57,6 @@ class BookmarkManager < Sinatra::Base
       flash[:notice] = "Sorry, your passwords don't match"
       erb :"users/new"
     end
-    # user = User.create(:email => params[:email],
-    #             :password => params[:password],
-    #             :password_confirmation => params[:password_confirmation])
-    # session[:user_id] = user.id
-    # redirect to('/')
   end
 
     helpers do
